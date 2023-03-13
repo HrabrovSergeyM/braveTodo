cff9846d8fa26318c6f6ef8a5cea5da78eb4b4ec
HrabrovSergeyM <69889536+HrabrovSergeyM@users.noreply.github.com>
2023-03-13 08:53:54 +0400

    main: first implementation of Storage

--- braveTodo/ToDoListViewController.swift
+++ braveTodo/ToDoListViewController.swift
@@ -4,9 +4,15 @@ class ToDoListViewController: UITableViewController {
 
     var itemArray = ["Buy milk", "Visit my doctor", "Wash the dishes"]
     
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+            itemArray = items
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -41,8 +47,9 @@ class ToDoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New ToDo Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             self.itemArray.append(textField.text ?? "New item")
+            
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             self.tableView.reloadData()
-            print(self.itemArray)
             
         }
         alert.addTextField { alertTextField in
