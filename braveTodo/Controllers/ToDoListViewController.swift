e331368a855efb79e3568fdb85677aed4c15f25e
HrabrovSergeyM <69889536+HrabrovSergeyM@users.noreply.github.com>
2023-03-14 05:20:07 +0400

    main: Implemented search functionality

--- braveTodo/Controllers/ToDoListViewController.swift
+++ braveTodo/Controllers/ToDoListViewController.swift
@@ -10,9 +10,23 @@ class ToDoListViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let appearance = UINavigationBarAppearance()
+
+//        appearance.configureWithTransparentBackground()
+
+//        appearance.backgroundColor = UIColor.systemBlue
+
+//        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+
+        navigationItem.standardAppearance = appearance
+
+        navigationItem.scrollEdgeAppearance = appearance
+        
         loadItems()
         
     }
+    
+    // MARK: - Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -24,16 +38,21 @@ class ToDoListViewController: UITableViewController {
         
         let item = itemArray[indexPath.row]
         
-        cell.textLabel?.text = item.title
+        cell.textLabel?.text = item.done == true ? "Completed" : item.title
 
         cell.accessoryType = item.done == true ? .checkmark : .none
         
         return cell
     }
 
+    // MARK: - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+     
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        context.delete(itemArray[indexPath.row])
+        itemArray.remove(at: indexPath.row)
         
         saveItems()
         
@@ -46,14 +65,11 @@ class ToDoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New ToDo Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             
-//            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-            
             let newItem = Item(context: self.context)
             newItem.title = textField.text ?? "New item"
             newItem.done = false
-            self.itemArray.append(newItem)
             
-            let encoder = PropertyListEncoder()
+            self.itemArray.append(newItem)
             
             self.saveItems()
             
@@ -80,15 +96,42 @@ class ToDoListViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-    func loadItems() {
-        let request: NSFetchRequest<Item> = Item.fetchRequest()
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
         
         do {
            itemArray = try context.fetch(request)
         } catch {
             print("Error reading context \(error)")
         }
+        
+        self.tableView.reloadData()
     }
+    
 }
 
+// MARK: - Search bar methods
+
+extension ToDoListViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request)
+        
+        if searchBar.text?.count == 0 {
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
+        
+    }
+    
+    
+}
 
